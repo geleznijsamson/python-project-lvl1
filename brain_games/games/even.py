bfb6da3f71ea91f9ac1a64e614d61834db61d264
Dmitrij Sergeev <geleznijsamson@gmail.com>
2022-06-19 19:56:51 +0300

    step_6.2

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -6,12 +6,20 @@ from random import randint
 def even_game():
     greet.greeting()
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    for i in range(0, 3):
+    i = 0
+    while i < 3:
         x = randint(1, 100)
+        i += 1
         if x % 2 == 0:
             y = 'yes'
             logic.structure(x, y)
+            continue
         elif x % 2 != 0:
             y = 'no'
             logic.structure(x, y)
+            continue
+        else:
+            y = True
+            logic.structure(x, y)
+            return
     print('Congratulation!')
