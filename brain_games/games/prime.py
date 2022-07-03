2b4ed86c0f44af41ebf4daf362e947aeeac6ea15
Dmitrij Sergeev <geleznijsamson@gmail.com>
2022-07-03 15:36:37 +0300

    fix_lint

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -6,7 +6,7 @@ def calculate():
     question = randint(1, 100)
     print(f'Question: {question}')
     count = 0
-    for i in range(2, question // 2+1):
+    for i in range(2, question // 2 + 1):
         if question % i == 0:
             count += 1
     if count <= 0:
