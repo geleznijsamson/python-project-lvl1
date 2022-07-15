aae1aca34e335fbf7bb04d5a0e605d3c8e3908b7
Dmitrij Sergeev <geleznijsamson@gmail.com>
2022-07-15 10:28:33 +0300

    fix_3

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,7 +4,8 @@ MANUAL = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def check_for_simplicity(question):
     for i in range(2, question // 2 + 1):
-        return question % i == 0
+        if question % i == 0:
+            return True
 
 
 def get_question_and_answer():
