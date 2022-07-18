bc1bd261159e437984ed434d13c1cacf0a455be8
Dmitrij Sergeev <geleznijsamson@gmail.com>
2022-07-18 15:41:14 +0300

    fix_prime_2

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,11 +1,22 @@
 from random import randint
-from sympy import isprime
 
 
 MANUAL = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
+def is_prime(question):
+    if question != 2 and question % 2 == 0 or question <= 1:
+        return False
+    else:
+        div = 3
+        while div <= question // 2:
+            if question % div == 0:
+                return False
+            div += 2
+        return True
+
+
 def get_question_and_answer():
     question = randint(1, 100)
-    answer = 'yes' if isprime(question) else 'no'
+    answer = 'yes' if is_prime(question) else 'no'
     return question, answer
